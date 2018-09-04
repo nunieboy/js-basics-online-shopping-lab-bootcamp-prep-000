1f5a8121adf40edea3aeac3803831fb1653295d7
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-04 15:57:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function placeOrder(cardNumber) {
   } else {
     var newTotal = total()
     cart.slice(0, cart.length +1 )
-    return `Your total cost is ${newTotal}, which will be charged to the card ${cardNumber}` 
+    return `Your total cost is \$${newTotal}, which will be charged to the card ${cardNumber}` 
   }
 }
 
