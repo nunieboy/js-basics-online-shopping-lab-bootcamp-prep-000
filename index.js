94774afcb8d62bd38dd64a4be95df1c05c3ab468
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-04 15:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === false){
     return ` Sorry, we don't have a credit card on file for you.`
   } else {
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}` cart.slice
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}` cart.slice(0, cart.length +1 )
   }
 }
 
